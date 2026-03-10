c8147f464bacc5feb293149f417aa721ca4fe3c7
vu minh duc-2280600748 <vuminhduc27052004@gmail.com>
2026-03-10 10:50:19 +0700

    sub, a, b

---
+++ sub.py
@@ -0,0 +1,12 @@
+def sub(a, b):
+    """Hàm trả về hiệu của a và b"""
+    return a - b
+
+if __name__ == "__main__":
+    print("--- CHƯƠNG TRÌNH TÍNH HIỆU ---")
+    # Nhập dữ liệu và ép kiểu sang số thực (float)
+    a = float(input("Mời bạn nhập số a: "))
+    b = float(input("Mời bạn nhập số b: "))
+    
+    # In kết quả
+    print(f"Kết quả phép trừ {a} - {b} là: {sub(a, b)}")
